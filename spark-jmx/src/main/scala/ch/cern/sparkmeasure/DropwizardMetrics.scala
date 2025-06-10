63f90216db8a714de59b9841a0a1a927b75e052e
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-06-10 12:00:20 +0200

    Update current feature

---
+++ spark-jmx/src/main/scala/ch/cern/sparkmeasure/DropwizardMetrics.scala
@@ -0,0 +1,31 @@
+package ch.cern.metrics
+
+import com.codahale.metrics.{Gauge, MetricRegistry}
+import com.codahale.metrics.jmx.JmxReporter
+
+object DropwizardMetrics {
+  val registry = new MetricRegistry()
+
+  // Simple cache local pour éviter double registration
+  private val knownMetrics = scala.collection.mutable.Set[String]()
+
+  // Démarre le JMX reporter une seule fois
+  private val reporter: JmxReporter = JmxReporter
+    .forRegistry(registry)
+    .inDomain("custom.metrics") // <== domaine JMX
+    .build()
+
+  reporter.start()
+
+  def setGauge(name: String, value: Double): Unit = {
+    if (!knownMetrics.contains(name)) {
+      registry.register(name, new Gauge[Double] {
+        override def getValue: Double = values.getOrElse(name, 0.0)
+      })
+      knownMetrics += name
+    }
+    values.update(name, value)
+  }
+
+  private val values = scala.collection.concurrent.TrieMap[String, Double]()
+}
