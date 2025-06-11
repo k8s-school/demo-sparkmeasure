45830e88b500283727f8c024aaac40b9644d618d
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-06-11 17:03:01 +0200

    Update current feature

--- spark-jmx/src/main/scala/ch/cern/sparkmeasure/DropwizardMetrics.scala
+++ spark-jmx/src/main/scala/ch/cern/sparkmeasure/DropwizardMetrics.scala
@@ -1,10 +1,26 @@
 package ch.cern.metrics
 
+import java.io.File
+
+import scala.io.Source
+
 import com.codahale.metrics.{Gauge, MetricRegistry}
 import com.codahale.metrics.jmx.JmxReporter
 
 object DropwizardMetrics {
   val registry = new MetricRegistry()
+
+  private def getNamespace(): String = {
+    val path = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+    val file = new File(path)
+    if (file.exists && file.canRead)
+      Source.fromFile(file).getLines().mkString.trim
+    else "unknown"
+  }
+
+  private def getPodName(): String = {
+    sys.env.getOrElse("HOSTNAME", "unknown")
+  }
 
   // Simple cache local pour éviter double registration
   private val knownMetrics = scala.collection.mutable.Set[String]()
@@ -18,7 +34,7 @@ object DropwizardMetrics {
   reporter.start()
 
   def setGauge(shortname: String, value: Double): Unit = {
-    val name = "namespace.pod." + shortname
+    val name = getNamespace() + "." + getPodName() + "." + shortname
     if (!knownMetrics.contains(name)) {
       registry.register(name, new Gauge[Double] {
         override def getValue: Double = values.getOrElse(name, 0.0)
